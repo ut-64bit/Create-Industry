76bf65e331a81aadbf3748208dc29ad7559c8fcf
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-06-22 10:13:11 +0900

    startup_item change

--- kubejs/server_scripts/startup_item.js
+++ kubejs/server_scripts/startup_item.js
@@ -4,6 +4,6 @@ onEvent('player.logged_in', event => {
     if (!event.player.stages.has('starting_items')) {
         event.player.stages.add('starting_items')
         // Give some items to player
-        event.player.give('minecraft:stone_sword')
+        event.player.give(`farmersdelight:flint_knife`)
     }
 })
