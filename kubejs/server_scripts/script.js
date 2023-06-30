469d2fbd81d9204446c4d02f9ca3754e06248193
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-06-30 14:07:25 +0900

    script update - armor trim support - delete recipes

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -195,6 +195,20 @@ onEvent(`recipes`, event => {
 	event.replaceOutput({ id: `alloyed:mixing/steel_ingot` }, `alloyed:steel_ingot`, Fluid.of(TC(`molten_steel`), 270))
 	event.replaceOutput({ id: `create:mixing/brass_ingot` }, `create:brass_ingot`, Fluid.of(TC(`molten_brass`), 180))
 
+	/* 一部の歯車のレシピを削除 */
+	event.remove({id:`extendedgears:smelting/half_shaft_steel_cogwheel_from_iron`})
+	event.remove({id:`extendedgears:smelting/half_shaft_steel_cogwheel_from_steel`})
+	event.remove({id:`extendedgears:smelting/large_half_shaft_steel_cogwheel_from_iron`})
+	event.remove({id:`extendedgears:smelting/large_half_shaft_steel_cogwheel_from_steel`})
+	event.remove({id:`extendedgears:smelting/shaftless_steel_cogwheel_from_iron`})
+	event.remove({id:`extendedgears:smelting/shaftless_steel_cogwheel_from_steel`})
+	event.remove({id:`extendedgears:smelting/large_shaftless_steel_cogwheel_from_iron`})
+	event.remove({id:`extendedgears:smelting/large_shaftless_steel_cogwheel_from_steel`})
+	event.remove({id:`extendedgears:smelting/steel_cogwheel_from_iron`})
+	event.remove({id:`extendedgears:smelting/steel_cogwheel_from_steel`})
+	event.remove({id:`extendedgears:smelting/large_steel_cogwheel_from_iron`})
+	event.remove({id:`extendedgears:smelting/large_steel_cogwheel_from_steel`})
+
 	/* 液体⇄インゴット */
 	let melt = (output, item, gem) => {
 		if (gem) {
