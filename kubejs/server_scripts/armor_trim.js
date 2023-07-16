e37cdf63472a2ef1c153277ac38c6a53412cd2bd
UT_64bit <83690503+ut-64bit@users.noreply.github.com>
2023-07-16 17:16:22 +0900

    update

--- kubejs/server_scripts/armor_trim.js
+++ kubejs/server_scripts/armor_trim.js
@@ -1,45 +1,47 @@
 // priority: 1
 
-const armor_trims = true
+if (Item.exists(`armor_trims:netherite_upgrade_smithing_template`)) {
+	onEvent(`recipes`, event => {
+		// #region func
+		let trim = (base, trim) => {
+			let inter = `kubejs:incomplete_${trim}_armor_trim_smithing_template`
+			event.recipes.create.sequencedAssembly(`armor_trims:${trim}_armor_trim_smithing_template`, `${base}`, [
+				event.recipes.create.deploying(inter, [inter, `armor_trims:${trim}_armor_trim_smithing_template`]).keepHeldItem(),
+				event.recipes.create.pressing(inter, inter),
+				event.recipes.create.filling(inter, [inter, Fluid.of(`tconstruct:molten_diamond`, 100)]),
+				event.recipes.create.filling(inter, [inter, Fluid.of(`tconstruct:molten_diamond`, 100)]),
+				event.recipes.create.pressing(inter, inter)
+			]).transitionalItem(`${inter}`).loops(1)
+		}
+		// #endregion
 
-onEvent(`recipes`, event => {
-    if (!armor_trims) return
+		// netherite_upgrade
+		let trans = `kubejs:incomplete_netherite_upgrade_smithing_template`
+		event.recipes.create.sequencedAssembly(`armor_trims:netherite_upgrade_smithing_template`, `netherrack`, [
+			event.recipes.create.deploying(trans, [trans, `armor_trims:netherite_upgrade_smithing_template`]).keepHeldItem(),
+			event.recipes.create.pressing(trans, trans),
+			event.recipes.create.filling(trans, [trans, Fluid.of(`tconstruct:molten_diamond`, 100)]),
+			event.recipes.create.filling(trans, [trans, Fluid.of(`tconstruct:molten_diamond`, 100)]),
+			event.recipes.create.pressing(trans, trans)
+		]).transitionalItem(trans).loops(1)
 
-    let trim = (base, trim) => {
-        let inter = `kubejs:incomplete_${trim}_armor_trim_smithing_template`
-        event.recipes.create.sequencedAssembly(`armor_trims:${trim}_armor_trim_smithing_template`, `${base}`, [
-            event.recipes.create.deploying(inter, [inter, `armor_trims:${trim}_armor_trim_smithing_template`]).keepHeldItem(),
-            event.recipes.create.pressing(inter, inter),
-            event.recipes.create.filling(inter, [inter, Fluid.of(`tconstruct:molten_diamond`, 100)]),
-            event.recipes.create.filling(inter, [inter, Fluid.of(`tconstruct:molten_diamond`, 100)]),
-            event.recipes.create.pressing(inter, inter)
-        ]).transitionalItem(`${inter}`).loops(1)
-    }
-    // armor_trims
-    /* 強化と装飾 */
-    let trans = `kubejs:incomplete_netherite_upgrade_smithing_template`
-    event.recipes.create.sequencedAssembly(`armor_trims:netherite_upgrade_smithing_template`, `netherrack`, [
-        event.recipes.create.deploying(trans, [trans, `armor_trims:netherite_upgrade_smithing_template`]).keepHeldItem(),
-        event.recipes.create.pressing(trans, trans),
-        event.recipes.create.filling(trans, [trans, Fluid.of(`tconstruct:molten_diamond`, 100)]),
-        event.recipes.create.filling(trans, [trans, Fluid.of(`tconstruct:molten_diamond`, 100)]),
-        event.recipes.create.pressing(trans, trans)
-    ]).transitionalItem(trans).loops(1)
-
-    trim(`#forge:cobblestone/normal`, `coast`)
-    trim(`#forge:sandstone`, `dune`)
-    trim(`#forge:endstone`, `eye`)
-    trim(`terracotta`, `host`)
-    trim(`terracotta`, `raiser`)
-    trim(`netherrack`, `rib`)
-    trim(`#forge:cobblestone/normal`, `sentry`)
-    trim(`terracotta`, `shaper`)
-    trim(`#forge:cobblestone/deepslate`, `silence`)
-    trim(`blackstone`, `snout`)
-    trim(`purpur_block`, `spire`)
-    trim(`prismarine`, `tide`)
-    trim(`#forge:cobblestone/normal`, `vex`)
-    trim(`#forge:cobblestone/deepslate`, `ward`)
-    trim(`terracotta`, `wayfinder`)
-    trim(`#forge:cobblestone/mossy`, `wild`)
-})
+		// #region trims
+		trim(`#forge:cobblestone/normal`, `coast`)
+		trim(`#forge:sandstone`, `dune`)
+		trim(`#forge:endstone`, `eye`)
+		trim(`terracotta`, `host`)
+		trim(`terracotta`, `raiser`)
+		trim(`netherrack`, `rib`)
+		trim(`#forge:cobblestone/normal`, `sentry`)
+		trim(`terracotta`, `shaper`)
+		trim(`#forge:cobblestone/deepslate`, `silence`)
+		trim(`blackstone`, `snout`)
+		trim(`purpur_block`, `spire`)
+		trim(`prismarine`, `tide`)
+		trim(`#forge:cobblestone/normal`, `vex`)
+		trim(`#forge:cobblestone/deepslate`, `ward`)
+		trim(`terracotta`, `wayfinder`)
+		trim(`#forge:cobblestone/mossy`, `wild`)
+		// #endregion
+	})
+}
